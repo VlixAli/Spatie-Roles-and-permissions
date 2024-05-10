3743e20a97a254851e7f44368a394a2812d01925
Ali <qali372@gmail.com>
2024-05-10 18:19:43 +0300

    feat : create and edit permissions

--- app/Http/Controllers/Admin/PermissionController.php
+++ app/Http/Controllers/Admin/PermissionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PermissionStoreRequest;
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Permission;
 
@@ -14,5 +15,31 @@ class PermissionController extends Controller
         return view('admin.permissions.index', [
             'permissions' => $permissions
         ]);
+    }
+
+    public function create()
+    {
+        return view('admin.permissions.create');
+    }
+
+    public function store(PermissionStoreRequest $request)
+    {
+        Permission::create($request->validated());
+
+        return to_route('admin.permissions.index');
+    }
+
+    public function edit(Permission $permission)
+    {
+        return view('admin.permissions.edit',[
+            'permission' => $permission
+        ]);
+    }
+
+    public function update(PermissionStoreRequest $request, Permission $permission)
+    {
+        $permission->update($request->validated());
+
+        return to_route('admin.permissions.index');
     }
 }
