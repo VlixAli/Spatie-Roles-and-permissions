48c9ebdc92469a95d2206c3d65443c784dd26b2d
Ali <qali372@gmail.com>
2024-05-10 18:19:23 +0300

    feat : create and edit roles

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RoleStoreRequest;
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Role;
 
@@ -14,5 +15,31 @@ class RoleController extends Controller
         return view('admin.roles.index', [
             'roles' => $roles
         ]);
+    }
+
+    public function create()
+    {
+        return view('admin.roles.create');
+    }
+
+    public function store(RoleStoreRequest $request)
+    {
+        Role::create($request->validated());
+
+        return to_route('admin.roles.index');
+    }
+
+    public function edit(Role $role)
+    {
+        return view('admin.roles.edit',[
+            'role' => $role
+        ]);
+    }
+
+    public function update(RoleStoreRequest $request, Role $role)
+    {
+        $role->update($request->validated());
+
+        return to_route('admin.roles.index');
     }
 }
